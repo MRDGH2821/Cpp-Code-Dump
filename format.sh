97f7abfc2a380b6f36baa09b91706f77627a8892
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-07-04 06:54:56 +0000

    chore: change extension to format

--- format.sh
+++ format.sh
@@ -2,12 +2,12 @@
 
 echo -----------------------------------
 echo "Formatting with Astyle"
-astyle --style=google --indent=spaces=2 --indent-classes --indent-modifiers --indent-switches --indent-cases --indent-namespaces --indent-preproc-block --indent-col1-comments --min-conditional-indent=2 --break-blocks=all --delete-empty-lines --break-one-line-headers --fill-empty-lines --suffix=none --add-braces --attach-return-type --mode=c --lineend=linux --recursive "./src/*.c"
+astyle --style=google --indent=spaces=2 --indent-classes --indent-modifiers --indent-switches --indent-cases --indent-namespaces --indent-preproc-block --indent-col1-comments --min-conditional-indent=2 --break-blocks=all --delete-empty-lines --break-one-line-headers --fill-empty-lines --suffix=none --add-braces --attach-return-type --mode=c --lineend=linux --recursive "./src/*.cpp"
 
 echo -----------------------------------
 echo "Formatting with Clang tidy"
-clang-tidy --fix --fix-errors ./src/*.c
+clang-tidy --fix --fix-errors ./src/*.cpp
 
 echo -----------------------------------
 echo "Formatting with Clang format"
-clang-format -i ./src/*.c
+clang-format -i ./src/*.cpp
